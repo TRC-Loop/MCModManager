045a82e7b78d6d2d042ceb7ba57b852baa1893b7
TRC-Loop <96308166+TRC-Loop@users.noreply.github.com>
2022-08-09 23:19:59 +0200

    Main Source Code Upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,168 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Das Formular überschreibt den Löschvorgang, um die Komponentenliste zu bereinigen.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Wird vom Windows Form-Designer benötigt.
+    Private components As System.ComponentModel.IContainer
+
+    'Hinweis: Die folgende Prozedur ist für den Windows Form-Designer erforderlich.
+    'Das Bearbeiten ist mit dem Windows Form-Designer möglich.  
+    'Das Bearbeiten mit dem Code-Editor ist nicht möglich.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.ToolStripLabel1 = New System.Windows.Forms.ToolStripLabel()
+        Me.ToolStripSeparator1 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ToolStripLabel2 = New System.Windows.Forms.ToolStripLabel()
+        Me.ToolStripSeparator2 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ToolStripButton1 = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripSeparator3 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ToolStripButton2 = New System.Windows.Forms.ToolStripButton()
+        Me.ToolStripSeparator4 = New System.Windows.Forms.ToolStripSeparator()
+        Me.ToolStripButton3 = New System.Windows.Forms.ToolStripButton()
+        Me.timelblrefreh = New System.Windows.Forms.Timer(Me.components)
+        Me.modrefresher = New System.Windows.Forms.Timer(Me.components)
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.OpenFileDialog1 = New System.Windows.Forms.OpenFileDialog()
+        Me.ToolStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom
+        Me.ToolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripLabel1, Me.ToolStripSeparator1, Me.ToolStripLabel2, Me.ToolStripSeparator2, Me.ToolStripButton1, Me.ToolStripSeparator3, Me.ToolStripButton2, Me.ToolStripSeparator4, Me.ToolStripButton3})
+        Me.ToolStrip1.Location = New System.Drawing.Point(0, 852)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(1197, 25)
+        Me.ToolStrip1.TabIndex = 1
+        Me.ToolStrip1.Text = "ToolStrip1"
+        '
+        'ToolStripLabel1
+        '
+        Me.ToolStripLabel1.Name = "ToolStripLabel1"
+        Me.ToolStripLabel1.Size = New System.Drawing.Size(43, 22)
+        Me.ToolStripLabel1.Text = "Mods: "
+        '
+        'ToolStripSeparator1
+        '
+        Me.ToolStripSeparator1.Name = "ToolStripSeparator1"
+        Me.ToolStripSeparator1.Size = New System.Drawing.Size(6, 25)
+        '
+        'ToolStripLabel2
+        '
+        Me.ToolStripLabel2.Name = "ToolStripLabel2"
+        Me.ToolStripLabel2.Size = New System.Drawing.Size(39, 22)
+        Me.ToolStripLabel2.Text = "Time: "
+        '
+        'ToolStripSeparator2
+        '
+        Me.ToolStripSeparator2.Name = "ToolStripSeparator2"
+        Me.ToolStripSeparator2.Size = New System.Drawing.Size(6, 25)
+        '
+        'ToolStripButton1
+        '
+        Me.ToolStripButton1.Image = Global.MCMod_Manager.My.Resources.Resources.shortcut
+        Me.ToolStripButton1.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton1.Name = "ToolStripButton1"
+        Me.ToolStripButton1.Size = New System.Drawing.Size(125, 22)
+        Me.ToolStripButton1.Text = "Open mods Folder"
+        '
+        'ToolStripSeparator3
+        '
+        Me.ToolStripSeparator3.Name = "ToolStripSeparator3"
+        Me.ToolStripSeparator3.Size = New System.Drawing.Size(6, 25)
+        '
+        'ToolStripButton2
+        '
+        Me.ToolStripButton2.Image = Global.MCMod_Manager.My.Resources.Resources.add
+        Me.ToolStripButton2.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton2.Name = "ToolStripButton2"
+        Me.ToolStripButton2.Size = New System.Drawing.Size(86, 22)
+        Me.ToolStripButton2.Text = "Add mod..."
+        '
+        'ToolStripSeparator4
+        '
+        Me.ToolStripSeparator4.Name = "ToolStripSeparator4"
+        Me.ToolStripSeparator4.Size = New System.Drawing.Size(6, 25)
+        '
+        'ToolStripButton3
+        '
+        Me.ToolStripButton3.Image = Global.MCMod_Manager.My.Resources.Resources.delete
+        Me.ToolStripButton3.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.ToolStripButton3.Name = "ToolStripButton3"
+        Me.ToolStripButton3.Size = New System.Drawing.Size(144, 22)
+        Me.ToolStripButton3.Text = "Delete Selected Mod..."
+        '
+        'timelblrefreh
+        '
+        Me.timelblrefreh.Enabled = True
+        Me.timelblrefreh.Interval = 1000
+        '
+        'modrefresher
+        '
+        Me.modrefresher.Enabled = True
+        Me.modrefresher.Interval = 5000
+        '
+        'ListBox1
+        '
+        Me.ListBox1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.Items.AddRange(New Object() {"Loading Installed Mods..."})
+        Me.ListBox1.Location = New System.Drawing.Point(0, 0)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(1197, 852)
+        Me.ListBox1.TabIndex = 2
+        '
+        'OpenFileDialog1
+        '
+        Me.OpenFileDialog1.Filter = "Mod Files|*.jar"
+        Me.OpenFileDialog1.Multiselect = True
+        Me.OpenFileDialog1.Title = "Select mods to add..."
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1197, 877)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.ToolStrip1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Minecraft Mod Manager"
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ToolStrip1 As ToolStrip
+    Friend WithEvents ToolStripLabel1 As ToolStripLabel
+    Friend WithEvents ToolStripSeparator1 As ToolStripSeparator
+    Friend WithEvents ToolStripLabel2 As ToolStripLabel
+    Friend WithEvents ToolStripSeparator2 As ToolStripSeparator
+    Friend WithEvents timelblrefreh As Timer
+    Friend WithEvents modrefresher As Timer
+    Friend WithEvents ToolStripButton1 As ToolStripButton
+    Friend WithEvents ToolStripSeparator3 As ToolStripSeparator
+    Friend WithEvents ToolStripButton2 As ToolStripButton
+    Friend WithEvents OpenFileDialog1 As OpenFileDialog
+    Friend WithEvents ToolStripSeparator4 As ToolStripSeparator
+    Friend WithEvents ToolStripButton3 As ToolStripButton
+    Public WithEvents ListBox1 As ListBox
+End Class
---
+++ Form1.vb
@@ -0,0 +1,138 @@
+﻿Imports System.IO
+Public Class Form1
+    Private Sub timelblrefreh_Tick(sender As Object, e As EventArgs) Handles timelblrefreh.Tick
+        ToolStripLabel2.Text = "Time: " + DateTime.Now.ToString("g")
+    End Sub
+
+    Private Sub modrefresher_Tick(sender As Object, e As EventArgs) Handles modrefresher.Tick
+        Dim path As String = Environ("USERPROFILE") + "\AppData\Roaming"
+        If System.IO.Directory.Exists(path + "\.minecraft") Then
+            If System.IO.Directory.Exists(path + "\.minecraft\mods") Then
+                Dim di As New IO.DirectoryInfo(path + "\.minecraft\mods")
+                Dim diar1 As IO.FileInfo() = di.GetFiles()
+                Dim dra As IO.FileInfo
+                ListBox1.Items.Clear()
+                ListBox1.BeginUpdate()
+                Dim i As Int64 = 0
+                'list the names of all files in the specified directory
+                For Each dra In diar1
+                    i += 1
+                    ListBox1.Items.Add(dra)
+                Next
+                ListBox1.EndUpdate()
+                ToolStripLabel1.Text = "Mods: " + i.ToString()
+            Else
+                System.IO.Directory.CreateDirectory(path + "\.minecraft\mods")
+            End If
+        Else
+            MsgBox("Error: Minecraft Not installed")
+        End If
+
+
+        'list the names of all files in the specified directory
+
+    End Sub
+
+    Private Sub ToolStripButton1_Click(sender As Object, e As EventArgs) Handles ToolStripButton1.Click
+        Process.Start(Environ("USERPROFILE") + "\AppData\Roaming\.minecraft\mods")
+    End Sub
+
+    Private Sub ToolStripButton2_Click(sender As Object, e As EventArgs) Handles ToolStripButton2.Click
+        OpenFileDialog1.Multiselect = True '// Allow Multiple File Selections.
+        If OpenFileDialog1.ShowDialog = DialogResult.OK Then
+            Dim pth As String = Environ("USERPROFILE") + "\AppData\Roaming"
+            For Each selFile As String In OpenFileDialog1.FileNames
+
+
+                Dim sourcepath As String = selFile
+                Dim DestPath As String = Environ("USERPROFILE") + "\AppData\Roaming\.minecraft\mods\"
+                Dim fn = Path.GetFileName(selFile)
+                My.Computer.FileSystem.CopyFile(
+                selFile,
+                DestPath + fn, overwrite:=True)
+            Next
+
+            Dim di As New IO.DirectoryInfo(pth + "\.minecraft\mods")
+            Dim diar1 As IO.FileInfo() = di.GetFiles()
+            Dim dra As IO.FileInfo
+            ListBox1.Items.Clear()
+            ListBox1.BeginUpdate()
+            Dim i As Int64 = 0
+            'list the names of all files in the specified directory
+            For Each dra In diar1
+                i += 1
+                ListBox1.Items.Add(dra)
+            Next
+            ListBox1.EndUpdate()
+            ToolStripLabel1.Text = "Mods: " + i.ToString()
+        End If
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        OpenFileDialog1.InitialDirectory = Environ("USERPROFILE") + "\Downloads"
+        Dim path As String = Environ("USERPROFILE") + "\AppData\Roaming"
+        If System.IO.Directory.Exists(path + "\.minecraft") Then
+            If System.IO.Directory.Exists(path + "\.minecraft\mods") Then
+                Dim di As New IO.DirectoryInfo(path + "\.minecraft\mods")
+                Dim diar1 As IO.FileInfo() = di.GetFiles()
+                Dim dra As IO.FileInfo
+                ListBox1.Items.Clear()
+                ListBox1.BeginUpdate()
+                Dim i As Int64 = 0
+                'list the names of all files in the specified directory
+                For Each dra In diar1
+                    i += 1
+                    ListBox1.Items.Add(dra)
+                Next
+                ListBox1.EndUpdate()
+                ToolStripLabel1.Text = "Mods: " + i.ToString()
+            Else
+                System.IO.Directory.CreateDirectory(path + "\.minecraft\mods")
+            End If
+        Else
+            MsgBox("Error: Minecraft Not installed")
+        End If
+        ToolStripLabel2.Text = "Time: " + DateTime.Now.ToString("g")
+    End Sub
+
+    Private Sub ToolStripButton3_Click(sender As Object, e As EventArgs) Handles ToolStripButton3.Click
+        If ListBox1.SelectedIndex = -1 Then
+            MsgBox("No Mod selected.")
+            Return
+        Else
+            Dim fl = Environ("USERPROFILE") + "\AppData\Roaming\.minecraft\mods\" + ListBox1.SelectedItem.ToString()
+            My.Computer.FileSystem.DeleteFile(
+            fl,
+            FileIO.UIOption.AllDialogs,
+            FileIO.RecycleOption.SendToRecycleBin,
+            FileIO.UICancelOption.ThrowException)
+
+            Dim path As String = Environ("USERPROFILE") + "\AppData\Roaming"
+            If System.IO.Directory.Exists(path + "\.minecraft") Then
+                If System.IO.Directory.Exists(path + "\.minecraft\mods") Then
+                    Dim di As New IO.DirectoryInfo(path + "\.minecraft\mods")
+                    Dim diar1 As IO.FileInfo() = di.GetFiles()
+                    Dim dra As IO.FileInfo
+                    ListBox1.Items.Clear()
+                    ListBox1.BeginUpdate()
+                    Dim i As Int64 = 0
+                    'list the names of all files in the specified directory
+                    For Each dra In diar1
+                        i += 1
+                        ListBox1.Items.Add(dra)
+                    Next
+                    ListBox1.EndUpdate()
+                    ToolStripLabel1.Text = "Mods: " + i.ToString()
+                Else
+                    System.IO.Directory.CreateDirectory(path + "\.minecraft\mods")
+                End If
+            Else
+                MsgBox("Error: Minecraft Not installed")
+            End If
+
+
+            'list the names of all files in the specified directory
+        End If
+
+    End Sub
+End Class
